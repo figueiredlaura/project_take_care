1981862f335ede8a5eb2f89d9e1d11808258f9ba
figueiredlaura <igueiredlaura5@gmail.com>
2024-04-16 20:46:00 -0300

    Modulando código e add navegação

---
+++ Screens/telasmenu.h
@@ -0,0 +1,11 @@
+#ifndef telasmenu_H
+#define telasmenu_H
+
+char telaprincipal(void);
+void telasobre(void);
+void teladev(void);
+char telamenucliente(void);
+char telaservicos(void);
+char tela_agendamentos(void);
+
+#endif
