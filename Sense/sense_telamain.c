8e22da52c8711bdcab6f39d49bc0e4718ec21f40
figueiredlaura <igueiredlaura5@gmail.com>
2024-04-17 20:58:04 -0300

    Add funções para melhorar navegacão

---
+++ Sense/sense_telamain.c
@@ -0,0 +1,8 @@
+#include "sense_telamain.h"
+#include "../Screens/telasmenu.h"
+#include "sense_menuclientes.h"
+#include "sense_menuservicos.h"
+#include "sense_menuagendamentos.h"
+#include <stdio.h>
+#include <stdlib.h>
+
