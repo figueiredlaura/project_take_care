b593ae685f78e09a0bb3a07c7033af3f207e46c5
figueiredlaura <igueiredlaura5@gmail.com>
2024-04-17 10:44:44 -0300

    Modulando e add navegação dos agendamentos

---
+++ Screens/telasagendamentos.h
@@ -0,0 +1,9 @@
+#ifndef telasagendamentos_H
+#define telasagendamentos_H
+
+void tela_agendamentos(void);
+void tela_pesquisaragendamentos(void);
+void tela_alteraragendamentos(void);
+void tela_excluiragendamentos(void);
+
+#endif
