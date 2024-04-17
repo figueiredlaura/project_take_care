1d7385d33681c955e928712ae4b1b64b7cbd3dc1
figueiredlaura <igueiredlaura5@gmail.com>
2024-04-16 21:22:04 -0300

    modulando e add navegações de clientes

---
+++ Screens/telasclientes.h
@@ -0,0 +1,9 @@
+#ifndef telasclientes_H
+#define telasclientes_H
+
+void telacadastrar_cliente(void);
+void telapesquisar_cliente(void);
+void telaalterar_cliente(void);
+void telaexcluir_cliente(void);
+
+#endif  
