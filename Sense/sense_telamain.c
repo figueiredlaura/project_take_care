cc4190ae8158190033fcbac3349f8589f01cc9a6
figueiredlaura <igueiredlaura5@gmail.com>
2024-04-17 21:18:57 -0300

    add funçoes para a navegação

--- Sense/sense_telamain.c
+++ Sense/sense_telamain.c
@@ -6,3 +6,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void sense_telamain(char option)
+{
+    switch (option)
+    {
+    
+    case '1':
+        sense_menuclientes();/* constant-expression */
+        /* code */
+        break;
+
+    case '2':
+        sense_menuservicos();
+        break;
+
+    case '3':
+        sense_menuagendamentos();
+
+    case '0':
+        system("clear||cls");
+        printf("\t   Agradecemos sua preferência.\n");
+        printf("\t   Volte sempre!! \n");
+        break;
+
+    default:
+        system("clear||cls");
+        printf("\t  Opção indisponível.TENTE NOVAMENTE.");
+
+        break;
+    }
+}
